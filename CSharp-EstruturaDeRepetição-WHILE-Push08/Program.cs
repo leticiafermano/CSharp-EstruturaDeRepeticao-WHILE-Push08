bd831f1d8bf3865313e888d3596e81a3c35dd0ba
Lethícia Santos <leticia-santos75@hotmail.com>
2022-02-17 20:03:46 -0300

    Adicionar arquivos de projeto.

---
+++ CSharp-EstruturaDeRepetição-WHILE-Push08/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_EstruturaDeRepetição_WHILE_Push08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
