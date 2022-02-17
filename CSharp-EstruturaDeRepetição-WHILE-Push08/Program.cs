ab17e3eb2a7dfff56a2a71620f2d2e296a4abe28
Lethícia Santos <leticia-santos75@hotmail.com>
2022-02-17 20:04:36 -0300

    CSharp Course Estrutura de repetição - WHILE

--- CSharp-EstruturaDeRepetição-WHILE-Push08/Program.cs
+++ CSharp-EstruturaDeRepetição-WHILE-Push08/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 
-namespace CSharp_EstruturaDeRepetição_WHILE_Push08
+namespace LogicaDeProgramacao_P5_WHILE
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             Estrutura de repetição:
+            REPITA/ ENQUANTO (WHILE)
+             */
+
+            int cont = 0;
+            while (cont < 20)  //Quantidade de repetição
+            {
+                Console.WriteLine("Letícia&Célio");
+                cont++; //Deverá ser colocado para que haja prosseguimento e adicione um valor ao 0, pra não roda infinitamente.
+            }
         }
     }
 }
